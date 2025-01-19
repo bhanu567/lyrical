0563eaab99b497c52d0408176a5672ceccb3db90
bhanu567 <bhanupjha5465@gmail.com>
2025-01-19 16:03:23 +0530

    successful build with mongoError resolve

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,11 +1,7 @@
 import mongoose from "mongoose";
 
-export async function connect(databaseName: string) {
+export async function getDatabaseConnection(mongoUrl: string) {
   try {
-    let mongoUrl = "";
-    if (databaseName === "music")
-      mongoUrl = process.env.MUSIC_COURSE_DATABASE_URL!;
-    else mongoUrl = process.env.USERS_DATABASE_URL!;
     await mongoose.connect(mongoUrl);
     const connection = mongoose.connection;
 
@@ -23,3 +19,58 @@ export async function connect(databaseName: string) {
     console.log(error);
   }
 }
+
+export function connect(databaseName: string) {
+  try {
+    const mongoUrl =
+      databaseName === "music"
+        ? process.env.MUSIC_COURSE_DATABASE_URL!
+        : process.env.USERS_DATABASE_URL!;
+
+    getDatabaseConnection(mongoUrl);
+  } catch (error) {
+    console.log("Something goes wrong! in Connect");
+    console.log(error);
+  }
+}
+// import mongoose, { Connection } from "mongoose";
+
+// const connections: Record<string, Connection> = {};
+
+// const getDatabaseConnection = async (
+//   databaseName: string,
+//   mongoUrl: string
+// ) => {
+//   try {
+//     if (!connections[databaseName]) {
+//       connections[databaseName] = await mongoose.createConnection(mongoUrl, {
+//         serverSelectionTimeoutMS: 30000,
+//         socketTimeoutMS: 45000,
+//       });
+
+//       connections[databaseName].on("connected", () =>
+//         console.log(`Connected to database: ${databaseName}`)
+//       );
+//       connections[databaseName].on("error", (err) =>
+//         console.error(`Error connecting to database ${databaseName}:`, err)
+//       );
+//     }
+//   } catch (error) {
+//     console.log("Something goes wrong!");
+//     console.log(error);
+//   }
+// };
+
+// export async function connect(databaseName: string) {
+//   try {
+//     const mongoUrl =
+//       databaseName === "music"
+//         ? process.env.MUSIC_COURSE_DATABASE_URL!
+//         : process.env.USERS_DATABASE_URL!;
+
+//     await getDatabaseConnection(databaseName, mongoUrl);
+//   } catch (error) {
+//     console.log("Something goes wrong! in Connect");
+//     console.log(error);
+//   }
+// }
